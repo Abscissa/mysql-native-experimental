d703878ecf16c8e06be9101681d8ad6f2d7fe11b
Nick Sabalausky <Abscissa@users.noreply.github.com>
2016-05-27 18:26:02 -0400

    GDC doesn't support -main

--- source/mysql/test/common.d
+++ source/mysql/test/common.d
@@ -32,6 +32,8 @@ debug(MYSQL_INTEGRATION_TESTS)
     import std.conv;
     import std.datetime;
 
+    void main() {}
+
     private @property string testConnectionStrFile()
     {
         import std.file, std.path;
