dfdcd6405ce253c5f8fb342d368ea673e47387cf
Nick Sabalausky <Abscissa@users.noreply.github.com>
2016-10-15 13:14:52 -0400

    GDC doesn't autocreate output dir.

--- run-phobos-tests.bat
+++ run-phobos-tests.bat
@@ -1,3 +1,7 @@
 @echo off
+
+rem GDC doesn't autocreate the dir (and git doesn't beleive in empty dirs)
+mkdir bin >NUL 2>NUL
+
 echo Compiling Phobos-socket tests...
 rdmd --build-only -g -unittest -debug=MYSQL_INTEGRATION_TESTS -ofbin/mysqln-tests-phobos -Isource source/mysql/connection.d && echo Running Phobos-socket tests... && bin/mysqln-tests-phobos
