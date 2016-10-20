bbdabf342f42d62e0316adff6d1a60f35626371a
Nick Sabalausky <Abscissa@users.noreply.github.com>
2016-10-20 15:24:41 -0400

    Compiling the tests should start at package.d, not connection.d

--- run-phobos-tests.bat
+++ run-phobos-tests.bat
@@ -4,4 +4,4 @@ rem GDC doesn't autocreate the dir (and git doesn't beleive in empty dirs)
 mkdir bin >NUL 2>NUL
 
 echo Compiling Phobos-socket tests...
-rdmd --build-only -g -unittest -debug=MYSQL_INTEGRATION_TESTS -ofbin/mysqln-tests-phobos -Isource source/mysql/connection.d && echo Running Phobos-socket tests... && bin/mysqln-tests-phobos
+rdmd --build-only -g -unittest -debug=MYSQL_INTEGRATION_TESTS -ofbin/mysqln-tests-phobos -Isource source/mysql/package.d && echo Running Phobos-socket tests... && bin/mysqln-tests-phobos
